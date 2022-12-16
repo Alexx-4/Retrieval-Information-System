b8e49548cc08b2d6b86a53a98b70445755ce2576
Ale <Ale@DESKTOP-KQJNTSU>
2022-12-16 10:57:20 -0500

    updating code

---
+++ backend/documents/clustering.py
@@ -0,0 +1,88 @@
+import math
+import pandas 
+import nltk
+import re
+import os
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+
+def clustering(path):
+    titles = []
+    content = []
+    empty_collection = False
+    dict_docs_line = {}
+
+    if os.path.isdir(path):
+        for root, _, files in os.walk(path):
+            if len(files) == 0:
+                return [],[],True,dict_docs_line
+            for filename in files:
+                with open(os.path.join(root,filename), encoding='utf8', errors='ignore') as f:
+                    titles.append(filename)
+                    lines = f.read()
+                    content.append(lines)
+
+                    first_line = lines.split('\n', 1)[0] + "..."
+                   
+                    dict_docs_line[filename] = first_line  
+
+    tfidf_vector = TfidfVectorizer(max_df=0.8, max_features=200000000, min_df=0.2, stop_words='english',use_idf=True,tokenizer=tokenize_stemmer,ngram_range=(1,3))
+    tfidf_matrix = tfidf_vector.fit_transform(content)
+
+ # metodo del codo
+    i = -1
+    inertia = []
+    for num_clusters in range(1,len(titles)):
+        kmeans = KMeans(n_clusters=num_clusters)
+        kmeans.fit(tfidf_matrix)
+        _sum = 0
+        for d in kmeans.transform(tfidf_matrix):
+            _sum += min(d)**2
+        
+        inertia.append(_sum)
+        
+        if len(inertia) > 1:
+            if math.fabs(inertia[i+1] - inertia[i]) < 15:
+                break        
+        
+        i+=1
+        
+    # analisis y devolucion de resultados
+    clusters = kmeans.labels_.tolist()
+
+    docs =  {    
+            'title' : titles, 
+            'text' : content,
+            'cluster': None
+            }
+    frame = pandas.DataFrame(docs,index=[clusters],columns=['title','cluster'])
+    frame['cluster'].value_counts()
+
+    doc_clust = {}
+    clust_docs = {}
+
+    for i in range(num_clusters):
+        doc_list = []
+        
+        _cluster = "Cluster" + str(i+1)
+             
+        for title in frame.loc[i]['title'].values.tolist():
+            doc_list.append(title)
+            doc_clust[title] = _cluster
+        
+        clust_docs[_cluster] = doc_list
+        
+    
+    return clust_docs, doc_clust, empty_collection, dict_docs_line
+    
+    
+def tokenize_stemmer(text):
+    stemmer = SnowballStemmer("english")
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    for token in tokens:
+        if re.search('[a-zA-Z]',token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
---
+++ main.py
@@ -0,0 +1,136 @@
+from operator import mod
+from flask import Flask, request, render_template,jsonify, redirect,url_for
+import webbrowser
+
+import os
+from backend.models.boolean_model import boolean_model
+from backend.documents.clustering import clustering
+from backend.models.vectorial_model import vectorial_model
+
+
+# VARIABLES
+_host = 'localhost'
+_port = 3002
+
+global _model, _collection
+global clust_docs, doc_clust
+global dict_docs_line
+
+
+# APP
+app = Flask(__name__)
+
+
+def initModel(model, collection):
+
+    global clust_docs, doc_clust
+    global dict_docs_line
+
+    # los documentos deben estar en la carpeta docs del directorio del proyecto
+    path = os.getcwd() + '/static/collections/' + collection
+
+    
+
+    # clustering 
+    clust_docs, doc_clust, empty_collection, dict_docs_line = clustering(path)
+
+    if model == 'vectorial':
+        # modelo vectorial
+        return vectorial_model(path),empty_collection
+    
+    elif model == 'booleano':
+        # modelo booleano
+        return boolean_model(path),empty_collection 
+    else: 
+        return '',empty_collection
+
+def getDocsDir():
+    base_dir = os.getcwd() + '/static/collections/'
+    with os.scandir(base_dir) as files:
+        subdirectories = [file.name for file in files if file.is_dir()]
+        return subdirectories
+
+
+
+def jsonResult(query):
+    
+    # umbral  (vectorial)
+    umbral = 0.11
+    # numero de documentos a devolver (booleano)
+    no_docs = 25
+   
+    json_result = ''
+    if isinstance(_model, vectorial_model):
+        json_result = _model._getResults(query, umbral)
+    else: 
+        json_result = _model._getResults(query, no_docs)
+    
+    return json_result
+
+
+# ROUTES
+@app.route('/', methods=["GET"])
+def home():
+
+    return render_template('home.html', subdirectories=getDocsDir()) 
+
+
+@app.route('/index', methods=["GET", "POST"])
+def index():
+    global _collection
+    global _model, dict_docs_line
+    
+    
+    if request.method == 'POST':
+        model = request.form['model']
+        collection = request.form['collection']
+
+        
+        _model,empty_collection = initModel(model, collection)
+
+        if empty_collection:
+            return render_template("error.html", msg="Selected collection is empty")
+        _collection = collection
+
+        
+        return render_template('index.html')
+
+    else:
+        return render_template('index.html')
+
+
+@app.route('/search', methods=["POST"])
+def search():
+    global clust_docs, doc_clust
+    global _model
+    global _collection, dict_docs_line
+
+    query = request.form['query']
+    results = jsonResult(query)['results']
+
+    docs = []
+    for i in range(0, len(results)): 
+        docs.append(results[i]['document'])
+    
+    return render_template('search.html', docs=docs, collection=_collection, query=query, clust_docs=clust_docs, doc_clust=doc_clust, dict_docs_line=dict_docs_line)
+
+
+
+
+@app.route('/cluster', methods=["POST"])
+def cluster():
+    global dict_docs_line
+    topic = request.form['topic']
+    docs_topic = clust_docs[topic]
+    return render_template('clusters.html', topic=topic, docs=docs_topic, collection=_collection, dict_docs_line=dict_docs_line)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("pagina_no_encontrada.html"), 404
+
+
+if __name__ == '__main__':
+    webbrowser.open(f'http://{_host}:{_port}', new=2)
+
+    app.run(host=_host, port=_port, debug=False)
